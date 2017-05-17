09359c2ad48c004f747073393bf64f385f837638
Domnich Evgeniy <domnich2016@gmail.com>
2017-05-17 06:58:23 +0300

    Add project files.

---
+++ HomeForkWriteToTheFile/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeForkWriteToTheFile
+{
+    class Program
+    {
+        static void Print2(DirectoryInfo[] info, int sub)
+        {
+            int i = 0;
+            foreach (DirectoryInfo dir in info)
+            {
+                Console.WriteLine($"{new string('-', sub)}{info[i].Name}");
+                i++;
+            }
+        }
+       public static int x = 1;
+
+        static void Method(DirectoryInfo[] info)
+        {           
+            foreach (DirectoryInfo dir in info)
+            {
+                Console.WriteLine($"{new string('-', x)}{dir}");
+                if (dir.GetDirectories() != null)
+                {
+                    x++;
+                    Method(dir.GetDirectories());
+                }
+                x --;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string waytodir = @"C:\Users\Domnich\Desktop\OOPGame-master";
+            var directory = new DirectoryInfo(waytodir);
+
+            DirectoryInfo[] dir = directory.GetDirectories();
+            Method(dir);
+
+        }
+    }
+}
+
+
+
---
+++ HomeWork1/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeForkWriteToTheFile
+{
+   
+     
+    class Program
+    {
+        public static void Write(string stroka)
+        {
+            using (StreamWriter file = new StreamWriter("Filee.txt", true))
+            {
+                file.WriteLine(stroka);
+            }
+        }
+
+        public static int x = 1;
+        static public int y = 0;
+        static void PrintFiles(FileInfo[] infofiles)
+        {
+            foreach (FileInfo name in infofiles)
+            {
+                y = x+1;
+                string n = $"{new string('-', y)}{name}";
+                Console.WriteLine(n);
+                Write(n);
+            }
+        }
+        static void Method(DirectoryInfo[] info)
+        {
+            foreach (DirectoryInfo dir in info)
+            {
+                string stroka = $"{new string('-', x)}{dir}";
+                Write(stroka);
+                Console.WriteLine(stroka);
+                if (dir.GetDirectories() != null)
+                {
+                    if (dir.GetFiles()!=null)
+                    {
+                        PrintFiles(dir.GetFiles());
+                    }
+                    x++;
+                    Method(dir.GetDirectories());
+                }
+                x--;
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            string waytodir = @"C:\Users\Domnich\Desktop\OOPGame-master";
+            var directory = new DirectoryInfo(waytodir);
+            Write(directory.Name);
+            DirectoryInfo[] dir = directory.GetDirectories();
+            Console.WriteLine(directory.Name);
+            Method(dir);
+
+            Console.ReadKey();
+                     
+        }
+    }
+}
